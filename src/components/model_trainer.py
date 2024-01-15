272ab912cccaa28e6d6f63dc60417bf4fee212fa
lakshyasoni97 <lakshyasoni97@gmail.com>
2024-01-15 23:38:59 +0530

    Hyperparameter Tuning 2

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -37,95 +37,50 @@ class ModelTrainer:
                 test_array[:, -1]              
             )
             
-            # models = {
-            #     "Random Forest" : RandomForestRegressor(),
-            #     "Decision Tree" : DecisionTreeRegressor(),
-            #     "Gradient Boosting" : GradientBoostingRegressor(),
-            #     "Linear Regression" : LinearRegression(),
-            #     "KNN Regression" : KNeighborsRegressor(),
-            #     "XGB Regression" : XGBRegressor(),                
-            #     "Catboosting Regressor" : CatBoostRegressor(verbose=False),
-            #     "Adaboost Regression" : AdaBoostRegressor()
-            # }
-
-            # params = {
-            #     "Decision Tree" : {
-            #         'criterion' : ['squared_error', 'friedman_mse', 'absolute_error', 'poisson']
-            #     },
-            #     'Random Forest' : {
-            #         'n_estimators' : [8, 16, 32, 64, 128, 256]
-            #     },
-            #     'Gradient Boosting' : {
-            #         'learning_rate' : [.1, .01, .05, .001],
-            #         'sub_sample' : [.6, .7, .75, .85, .8, .9],
-            #         'n_estimators' : [8, 16, 32, 64, 128, 256]
-            #     },
-            #     'Linear Regression' : {},
-            #     'KNN Regression' : {
-            #         'n_neighbors' : [5,7,9,11]
-            #     },
-            #     "XGB Regression" : {
-            #         'learning_rate' : [.1, .01, .05, .001],
-            #         'n_estimators' : [8, 16, 32, 64, 128, 256]
-            #     },
-            #     'Catboosting Regressor' : {
-            #         'depth' : [6,8,10],
-            #         'learning_rate' : [.1, .01, .05, .001],
-            #         'iterations' : [30, 50, 100]
-            #     },
-            #     'Adaboost Regression' : {
-            #         'learning_rate' : [.1, .01, .05, .001],
-            #         'n_estimators' : [8, 16, 32, 64, 128, 256]                    
-            #     }
-                
-            # }
-            
             models = {
-                "Random Forest": RandomForestRegressor(),
-                "Decision Tree": DecisionTreeRegressor(),
-                "Gradient Boosting": GradientBoostingRegressor(),
-                "Linear Regression": LinearRegression(),
-                "XGBRegressor": XGBRegressor(),
-                "CatBoosting Regressor": CatBoostRegressor(verbose=False),
-                "AdaBoost Regressor": AdaBoostRegressor(),
+                "Random Forest" : RandomForestRegressor(),
+                "Decision Tree" : DecisionTreeRegressor(),
+                "Gradient Boosting" : GradientBoostingRegressor(),
+                "Linear Regression" : LinearRegression(),
+                "KNN Regression" : KNeighborsRegressor(),
+                "XGB Regression" : XGBRegressor(),                
+                "Catboosting Regressor" : CatBoostRegressor(verbose=False),
+                "Adaboost Regression" : AdaBoostRegressor()
             }
-            params={
-                "Decision Tree": {
-                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
-                    # 'splitter':['best','random'],
-                    # 'max_features':['sqrt','log2'],
+
+            params = {
+                "Decision Tree" : {
+                    'criterion' : ['squared_error', 'friedman_mse', 'absolute_error', 'poisson']
                 },
-                "Random Forest":{
-                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
-                 
-                    # 'max_features':['sqrt','log2',None],
-                    'n_estimators': [8,16,32,64,128,256]
+                'Random Forest' : {
+                    'n_estimators' : [8, 16, 32, 64, 128, 256]
                 },
-                "Gradient Boosting":{
-                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
-                    'learning_rate':[.1,.01,.05,.001],
-                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
-                    # 'criterion':['squared_error', 'friedman_mse'],
-                    # 'max_features':['auto','sqrt','log2'],
-                    'n_estimators': [8,16,32,64,128,256]
+                'Gradient Boosting' : {
+                    'learning_rate' : [.1, .01, .05, .001],
+                    'subsample' : [0.6,0.7,0.75,0.8,0.85,0.9],
+                    'n_estimators' : [8, 16, 32, 64, 128, 256]
                 },
-                "Linear Regression":{},
-                "XGBRegressor":{
-                    'learning_rate':[.1,.01,.05,.001],
-                    'n_estimators': [8,16,32,64,128,256]
+                'Linear Regression' : {},
+                'KNN Regression' : {
+                    'n_neighbors' : [5,7,9,11]
                 },
-                "CatBoosting Regressor":{
-                    'depth': [6,8,10],
-                    'learning_rate': [0.01, 0.05, 0.1],
-                    'iterations': [30, 50, 100]
+                "XGB Regression" : {
+                    'learning_rate' : [.1, .01, .05, .001],
+                    'n_estimators' : [8, 16, 32, 64, 128, 256]
                 },
-                "AdaBoost Regressor":{
-                    'learning_rate':[.1,.01,0.5,.001],
-                    # 'loss':['linear','square','exponential'],
-                    'n_estimators': [8,16,32,64,128,256]
+                'Catboosting Regressor' : {
+                    'depth' : [6,8,10],
+                    'learning_rate' : [.1, .01, .05, .001],
+                    'iterations' : [30, 50, 100]
+                },
+                'Adaboost Regression' : {
+                    'learning_rate' : [.1, .01, .05, .001],
+                    'n_estimators' : [8, 16, 32, 64, 128, 256]                    
                 }
                 
             }
+            
+
             
             model_report:dict = evaluate_model(X_train = X_train, y_train = y_train,
                                                X_test = X_test, y_test = y_test, models = models, params = params)
